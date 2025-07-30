b4dabec49e98c5c01e8c30245701d8378eee2b0b
evankwan <evanyuekwan@gmail.com>
2025-07-30 13:53:44 -0400

    finish off promises section of 30 days of javascript

---
+++ medium/2721-execute-asynchronous-functions-in-parallel.ts
@@ -0,0 +1,29 @@
+type Fn<T> = () => Promise<T>
+
+function promiseAll<T>(functions: Fn<T>[]): Promise<T[]> {
+    return new Promise(function(res, rej) {
+        const results = []
+        let completed = 0
+
+        if (functions.length === 0) {
+            res(results)
+        }
+
+        functions.forEach(function(fn, i) {
+            fn()
+                .then(function(result) {
+                    results[i] = result
+                    completed++;
+
+                    if (completed === functions.length) {
+                        res(results)
+                    }
+                })
+                .catch(function(error) {
+                    rej(error)
+                })
+        })
+    })
+}
+
+export {}
