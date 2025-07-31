c939de99c36f907dd4b50d2b22c9ca0c179d3d77
evankwan <evanyuekwan@gmail.com>
2025-07-30 20:24:11 -0400

    add more JSON solutions

---
+++ medium/2625-flatten-deeply-nested-array.ts
@@ -0,0 +1,14 @@
+type MultiDimensionalArray = (number | MultiDimensionalArray)[];
+
+let flat = function (arr:  MultiDimensionalArray, n: number):  MultiDimensionalArray {
+    let array: (number | MultiDimensionalArray)[] = []
+    for (let i = 0; i < arr.length; i++) {
+        if (!Array.isArray(arr[i]) || n === 0) {
+            array.push(arr[i])
+            continue
+        }
+
+        array.push(...flat(arr[i] as MultiDimensionalArray, n - 1))
+    }
+    return array
+}
