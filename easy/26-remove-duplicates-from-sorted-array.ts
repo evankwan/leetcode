acdee7bdac653c44e928ecd9d88b6947bec0497f
evankwan <evanyuekwan@gmail.com>
2025-08-07 17:06:39 -0400

    couple more layups

---
+++ easy/26-remove-duplicates-from-sorted-array.ts
@@ -0,0 +1,14 @@
+function removeDuplicates(nums: number[]): number {
+    const uniqueNums: Set<number> = new Set()
+    for (let i = 0; i < nums.length; i++) {
+        if (uniqueNums.has(nums[i])) {
+            nums.splice(i, 1)
+            i--
+            continue
+        }
+
+        uniqueNums.add(nums[i])
+    }
+
+    return nums.length
+}
