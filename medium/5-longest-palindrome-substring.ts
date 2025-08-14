14b067e3651f527d891765d6cb09564a8941c826
evankwan <evanyuekwan@gmail.com>
2025-08-14 14:22:00 -0400

    another medium problem

---
+++ medium/5-longest-palindrome-substring.ts
@@ -0,0 +1,31 @@
+function isPalidrome(s: string) {
+    let isTrue = true;
+    for (let i = 0; i <= s.length / 2; i++) {
+        if (s[i] !== s[s.length - 1 - i]) {
+            isTrue = false
+            break
+        }
+    }
+    return isTrue
+}
+
+function longestPalindrome(s: string): string {
+    let longestString = ""
+    for (let i = 0; i < s.length; i++) {
+        let currentSubstring = ""
+        for (let j = i; j < s.length; j++) {
+            currentSubstring += s[j]
+
+            if (
+                currentSubstring.length > longestString.length
+                && isPalidrome(currentSubstring)
+            ) {
+                longestString = currentSubstring
+            }
+        }
+        if (longestString.length >= s.length - i) {
+            break
+        }
+    }
+    return longestString
+}
