433ea93a16de7dde457e6275a1db37267bd56c81
evankwan <evanyuekwan@gmail.com>
2025-08-20 13:41:01 -0400

    container with most water

---
+++ medium/11-container-with-most-water.ts
@@ -0,0 +1,16 @@
+function maxArea(height: number[]): number {
+    let low = 0
+    let high = height.length - 1
+    let max = 0
+    while (low <= high) {
+        const distance = Math.min(height[low], height[high])
+        const breadth = high - low
+        const area = distance * breadth
+
+        max = Math.max(max, area)
+
+        if (height[low] < height[high]) low++
+        else high--
+    }
+    return max
+};
