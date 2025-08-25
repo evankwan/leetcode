d9d4357ef906304acd859ea91b752f480e8cd5a8
evankwan <evanyuekwan@gmail.com>
2025-08-25 13:33:31 -0400

    couple more practice

---
+++ easy/100-same-tree.ts
@@ -0,0 +1,25 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+    const isCurSame = p?.val == q?.val
+
+    let isLeftSame = true
+    if (p?.left || q?.left) {
+        isLeftSame = isSameTree(p?.left ?? null, q?.left ?? null)
+    }
+
+    let isRightSame = true
+    if (p?.right || q?.right) {
+        isRightSame = isSameTree(p?.right ?? null, q?.right ?? null)
+    }
+    return isCurSame && isLeftSame && isRightSame
+};
---
+++ easy/101-symmetric-tree.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+
+function isSymmetric(root: TreeNode | null): boolean {
+    function isBranchSymmetric(left: TreeNode | null, right: TreeNode | null): boolean {
+        if (!left && !right) return true
+
+        if (left?.val !== right?.val) return false
+
+        return isBranchSymmetric(left?.left ?? null, right?.right ?? null)
+            && isBranchSymmetric(left?.right ?? null, right?.left ?? null)
+    }
+    return isBranchSymmetric(root?.left ?? null, root?.right ?? null)
+};
