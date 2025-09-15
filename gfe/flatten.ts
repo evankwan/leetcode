7ff4c54c93f755d2a1827fbb0fa8b6200be567e1
evankwan <evanyuekwan@gmail.com>
2025-09-15 18:22:30 -0400

    flatten

---
+++ gfe/flatten.ts
@@ -0,0 +1,19 @@
+type ArrayValue = any | Array<ArrayValue>;
+
+export default function flatten(array: Array<ArrayValue>): Array<any> {
+  let acc: any[] = []
+  // loop through the value
+  array.forEach((value) => {
+    console.log(value, !Array.isArray(value))
+    // if value is NOT an array, return it
+    if (!Array.isArray(value)) {
+      acc.push(value)
+      return
+    }
+
+    // if the value is an array, spread the attributes
+    acc.push(...flatten(value))
+  })
+
+  return acc
+}
