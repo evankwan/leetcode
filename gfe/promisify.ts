be9cdb7684672d9082b1ad03c3bf2a8de32dc3c3
evankwan <evanyuekwan@gmail.com>
2025-09-25 10:52:29 -0400

    format

--- gfe/promisify.ts
+++ gfe/promisify.ts
@@ -1,20 +1,19 @@
 export default function promisify<T>(
-    func: (...args: any[]) => void,
-  ): (this: any, ...args: any[]) => Promise<T> {
-    // return a function that taks the arguments
-    return function(...args) {
-      // create a promise
-      return new Promise((res, rej) => {
-        const cb = function (err: Error, val: any): any {
-          if (err) {
-            rej(err);
-          } else {
-            res(val);
-          }
-        };
-        // call the function with the args and the cb function
-        func.call(this, ...args, cb);
-      });
-    };
-  }
-  
+	func: (...args: any[]) => void
+): (this: any, ...args: any[]) => Promise<T> {
+	// return a function that taks the arguments
+	return function (...args) {
+		// create a promise
+		return new Promise<T>((res, rej) => {
+			// call the function with the args and the cb function
+			func.call(this, ...args, function (err: Error | null, val: T): void {
+				if (err) {
+					rej(err);
+					// test
+				} else {
+					res(val);
+				}
+			});
+		});
+	};
+}
